b5acc68eb844190954f22a3d78363bb84f20bca1
Alexey Emelchikov <128730329+FlexeyE98@users.noreply.github.com>
2023-08-06 23:35:20 +0300

    Update FriendsIncomingRequestView.cs

--- socialNetWork/SocialNetWork/PLL/Views/FriendsIncomingRequestView.cs
+++ socialNetWork/SocialNetWork/PLL/Views/FriendsIncomingRequestView.cs
@@ -11,21 +11,16 @@ namespace SocialNetWork.PLL.Views
         public void Show(IEnumerable<Friend> outcomingFriends)
         {
             Console.Write("Друзья: ");
-
-
             if (outcomingFriends.Count() == 0)
             {
                 Console.WriteLine("Друзей пока нет :(");
                 return;
             }
-
             outcomingFriends.ToList().ForEach(f =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("ID пользователя: {0}", f.FriendId);
                 Console.ForegroundColor = ConsoleColor.White;
-
-
             });
         }
     }
